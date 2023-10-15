05444f14524ef4de981c3bb4999e4eadeda07246
yx <364332625@qq.com>
2023-10-15 16:11:57 +0800

    pm2 config port change

--- server/pm2.config.js
+++ server/pm2.config.js
@@ -3,10 +3,9 @@ module.exports = {
         {
             name: '快传后端',
             cwd: '/root/code/snapdrop/server',
-            script: 'npm',
-            args: 'index.js',
+            script: './index.js',
             env: {
-
+                "PORT": 3332
             },
         },
     ],
