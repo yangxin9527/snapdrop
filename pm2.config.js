87baaaf9604d438afe03a188edd6fcbdfd16b5fc
yx <364332625@qq.com>
2023-10-15 15:26:13 +0800

    init ci

---
+++ pm2.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+        {
+            name: '快传后端',
+            cwd: '/root/code/snapdrop/server',
+            script: 'npm',
+            args: 'index.js',
+            env: {
+
+            },
+        },
+    ],
+};
